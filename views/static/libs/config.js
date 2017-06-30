8fcab5aea3c35eb8396f5f1faf288b80c5be099d
pan <panming@itcast.cn>
2017-06-30 16:48:03 +0800

    实现了讲师的编辑和添加功能

--- views/static/libs/config.js
+++ views/static/libs/config.js
@@ -5,7 +5,8 @@ require.config({
      	"form": "assets/jquery-form/jquery.form",
      	"cookie": "assets/jquery-cookie/jquery.cookie",
      	"template": "assets/artTemplate/template",
-     	"bootstrap": "assets/bootstrap/js/bootstrap.min"
+     	"bootstrap": "assets/bootstrap/js/bootstrap.min",
+     	"util": "static/js/util"
      },
      shim: {
      	"bootstrap": {
