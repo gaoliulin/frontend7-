ec9379e13eba1df64e1524c0a8118a8fc10d6954
pan <panming@itcast.cn>
2017-06-30 09:40:19 +0800

    完善了登录功能，用户在没有登录的情况下只能访问到登录页面，访问其他页面会自动跳转到登录页面

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -2,12 +2,17 @@ define(["jquery", "template", "cookie"], function($, template){
 	$(function(){
 		//判断当前页是不是在登录页面，如果是就不做如下的操作
 		if("/dashboard/login" != location.pathname){
-			//从cookie中获取登录成功后存储的用户信息
-	        var userInfo = JSON.parse($.cookie("userinfo"))
-	        var html = template("profile-tpl", userInfo);
+			//完善登录功能，当用户处于未登录状态的时候，需要让用户直接跳转到登录页面
+			if(!$.cookie("PHPSESSID")){
+				location.href = "/dashboard/login";
+			}else{
+				//从cookie中获取登录成功后存储的用户信息
+		        var userInfo = JSON.parse($.cookie("userinfo"))
+		        var html = template("profile-tpl", userInfo);
 
-	        //将模板渲染到页面中刚才挖坑的地方
-	        $("#userinfo").html(html);
+		        //将模板渲染到页面中刚才挖坑的地方
+		        $("#userinfo").html(html);
+			}
 		}  
 
 
@@ -24,5 +29,7 @@ define(["jquery", "template", "cookie"], function($, template){
 				}
 			})
 		});
+
+
     })
 })
