c94f8bcffadef63a0d65dfb99ecbc9ca64867e33
pan <panming@itcast.cn>
2017-07-02 15:29:27 +0800

    完善了讲师添加以及编辑的页面

--- views/static/libs/config.js
+++ views/static/libs/config.js
@@ -6,11 +6,20 @@ require.config({
      	"cookie": "assets/jquery-cookie/jquery.cookie",
      	"template": "assets/artTemplate/template",
      	"bootstrap": "assets/bootstrap/js/bootstrap.min",
-     	"util": "static/js/util"
+     	"util": "static/js/util",
+          "datepicker": "assets/bootstrap-datepicker/js/bootstrap-datepicker.min",
+          "datepicker-zh": "assets/bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min",
+          "validate": "assets/validate/jquery-validate.min"
      },
      shim: {
      	"bootstrap": {
      		deps: ["jquery"]
-     	}
+     	},
+          "datepicker-zh": {
+               deps: ["datepicker"]
+          },
+          "validate": {
+               deps: ["jquery"]
+          }
      }
 });
